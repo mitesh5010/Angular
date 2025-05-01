40e640d432f7e279df5d27839b93a3b8e0094f15
Mitesh <miteshprajapati6439.com>
2025-05-01 18:00:12 +0530

    learn decoraters and create task component

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
+import { DUMMY_USERS } from './dummy-users';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, TaskComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+  selectUserId = 'u1';
+
+  get selectUser(){
+    return this.users.find((user)=> user.id===this.selectUserId)!;
+  }
+
+
+  onSelectUser(id:string){
+    this.selectUserId = id;
+  }
+}
