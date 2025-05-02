f5875ae7bd379a850c8fd38df4e5940252f4da5e
Mitesh <miteshprajapati6439.com>
2025-05-02 14:54:46 +0530

    create task component for each user and learn about difference of !/?

--- 01-starting-project/src/app/app.component.ts
+++ 01-starting-project/src/app/app.component.ts
@@ -2,21 +2,22 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from "./user/user.component";
 import { DUMMY_USERS } from './dummy-users';
-import { TaskComponent } from './task/task.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent, TaskComponent],
+  imports: [HeaderComponent, UserComponent, TasksComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
-  selectUserId = 'u1';
+  selectUserId?:string;
 
   get selectUser(){
-    return this.users.find((user)=> user.id===this.selectUserId)!;
+    return this.users.find((user)=> user.id===this.selectUserId);
   }
 
 
