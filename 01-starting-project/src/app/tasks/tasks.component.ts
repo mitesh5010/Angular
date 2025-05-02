5d4b94753fd2adba4b4d7a3b8eae70a188912882
Mitesh <miteshprajapati6439.com>
2025-05-02 16:58:23 +0530

    create Add task component and cancle event

--- 01-starting-project/src/app/tasks/tasks.component.ts
+++ 01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,16 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { NewTaskComponent } from "./new-task/new-task.component";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, NewTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input({required:true}) userId!:string;
   @Input({required: true}) name!:string;
+  isAddingTask =false;
+
   tasks = [
     {
       id: 't1',
@@ -43,6 +46,14 @@ export class TasksComponent {
 
   onCompleteTask(id:string){
     this.tasks = this.tasks.filter((task)=>task.id !== id)
+  }
+
+  onStartAddTask(){
+    this.isAddingTask =true; 
+  }
+
+  onCancleTask(){
+    this.isAddingTask =false;
   }
 
 }
