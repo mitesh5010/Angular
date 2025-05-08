a2f1ac769fb16eb1d77d8bb4fc2db3206caefbbf
Mitesh <miteshprajapati6439.com>
2025-05-08 14:54:33 +0530

    create code to use modules

--- 02-PROJECT/src/app/investment-results/investment-results.component.ts
+++ 02-PROJECT/src/app/investment-results/investment-results.component.ts
@@ -1,11 +1,10 @@
-import { CurrencyPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
-  standalone: true,
-  imports: [CurrencyPipe],
+  standalone: false,
+  // imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css'
 })
