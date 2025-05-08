f2f5ee525e2136923d1ca9238ad447a80c3e7f0b
Mitesh <miteshprajapati6439.com>
2025-05-08 12:27:50 +0530

    display table include data of result

--- 02-PROJECT/src/app/user-input/user-input.component.ts
+++ 02-PROJECT/src/app/user-input/user-input.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { type InvestmentInput } from '../investment-input.model';
 
 @Component({
   selector: 'app-user-input',
@@ -10,16 +11,19 @@ import { FormsModule } from '@angular/forms';
 })
 export class UserInputComponent {
 
+  @Output() calculate = new EventEmitter<InvestmentInput>();
   enterInitialInvestment = '0';
   enterAnnualInterestRate = '0';
   enterExpectedReturn = '5';
   enterDuration = '10';
 
   onSubmit(){
-    console.log("Form submitted!");
-    console.log("Initial Investment: " + this.enterInitialInvestment);
-    console.log("Annual Interest Rate: " + this.enterAnnualInterestRate);
-    console.log("Expected Return: " + this.enterExpectedReturn);
-    console.log("Duration: " + this.enterDuration);
+    this.calculate.emit({
+      // Convert string inputs to numbers using the + plus operator
+      initialInvestment: +this.enterInitialInvestment,
+      annualInvestment: +this.enterAnnualInterestRate,
+      expectedReturn: +this.enterExpectedReturn,
+      duration: +this.enterDuration
+    });
   }
 }
