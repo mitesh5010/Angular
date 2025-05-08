21f55f8b24cd5c5704de0197f4e4765150dff5e4
Mitesh <miteshprajapati6439.com>
2025-05-08 11:23:42 +0530

    use two-way binding in form

--- 02-PROJECT/src/app/user-input/user-input.component.ts
+++ 02-PROJECT/src/app/user-input/user-input.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
 
+  enterInitialInvestment = '0';
+  enterAnnualInterestRate = '0';
+  enterExpectedReturn = '5';
+  enterDuration = '10';
+
+  onSubmit(){
+    console.log("Form submitted!");
+    console.log("Initial Investment: " + this.enterInitialInvestment);
+    console.log("Annual Interest Rate: " + this.enterAnnualInterestRate);
+    console.log("Expected Return: " + this.enterExpectedReturn);
+    console.log("Duration: " + this.enterDuration);
+  }
 }
