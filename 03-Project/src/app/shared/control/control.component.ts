8393ec1e376fce82ca81f1b5fd052fb776ab3505
Mitesh <miteshprajapati6439.com>
2025-05-12 11:12:27 +0530

    rely on Host element

--- 03-Project/src/app/shared/control/control.component.ts
+++ 03-Project/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, ViewEncapsulation } from '@angular/core';
+import { Component, Host, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -6,7 +6,10 @@ import { Component, input, ViewEncapsulation } from '@angular/core';
   imports: [],
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',  
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+  }
 })
 export class ControlComponent {
   label = input.required<string>();
