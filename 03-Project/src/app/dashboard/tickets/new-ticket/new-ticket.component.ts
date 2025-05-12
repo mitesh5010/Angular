ca0519c71e8787b137829bb4cc40f8ec48f38ab9
Mitesh <miteshprajapati6439.com>
2025-05-12 18:09:20 +0530

    learn viewChild / contentChild

--- 03-Project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ 03-Project/src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, viewChild, ViewChild } from '@angular/core';
 import { ControlComponent } from "../../../shared/control/control.component";
 import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent, ButtonComponent],
+  imports: [ControlComponent, ButtonComponent,FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+  private form = viewChild.required<ElementRef<HTMLFormElement>>('form')
 
+  onSubmit(title: string, ticketText: string) {
+    console.log(title + ' : ' + ticketText);
+    this.form().nativeElement.reset();
+  }
 }
--- 03-Project/src/app/shared/control/control.component.ts
+++ 03-Project/src/app/shared/control/control.component.ts
@@ -1,4 +1,4 @@
-import { Component, Host, HostBinding, HostListener, input, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, contentChild, ElementRef, Host, HostBinding, HostListener, inject, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -17,7 +17,16 @@ export class ControlComponent {
   // @HostListener('click') onClick() {
   //   console.log('Control clicked');
   // }
+  // @ContentChild('input') private control?: ElementRef<HTMLInputElement | HTMLTextAreaElement>;
+
+  private control = contentChild<ElementRef<HTMLInputElement | HTMLTextAreaElement>>('input')
 
   label = input.required<string>();
-  
+  private el = inject(ElementRef);
+  onClick() {
+    console.log('Control clicked');
+    console.log(this.el);
+    console.log(this.control());
+    
+  }
 }
