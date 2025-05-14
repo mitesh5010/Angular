789db936c970fd18c50e2cfde265b1ed00254f7b
Mitesh <miteshprajapati6439.com>
2025-05-14 12:49:37 +0530

    create custom pipe and done with Project-05

--- 05-PROJECT/src/app/app.component.ts
+++ 05-PROJECT/src/app/app.component.ts
@@ -1,9 +1,14 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
+
+import { TemperaturePipe } from './temprature.pipe';
+import { SortPipe } from './sort.pipe';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
+  imports: [DatePipe , TemperaturePipe, SortPipe],
 })
 export class AppComponent {
   currentDate = new Date();
@@ -14,11 +19,18 @@ export class AppComponent {
     chicago: 65.0775238,
   };
 
+  constructor() {
+    this.historicTemperatures.sort((a, b) => a>b ? 1 :-1);
+  }
+
   historicTemperatures = [
     25, 37, 19, -4, 28, 21, 19, 28, 33, 31, 9, 11, 5, -12, -5,
   ];
 
   onReset(index: number) {
+    // const newTemp = [...this.historicTemperatures];
+    // newTemp[index] = 18;
+    // this.historicTemperatures = newTemp;
     this.historicTemperatures[index] = 18;
   }
 }
