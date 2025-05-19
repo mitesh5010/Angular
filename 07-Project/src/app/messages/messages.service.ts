2d47c22e997a1be5d6f8db5f5f8dcb4b99ca1c43
Mitesh <miteshprajapati6439.com>
2025-05-19 15:58:46 +0530

    change detection manually using RxJS subject

--- 07-Project/src/app/messages/messages.service.ts
+++ 07-Project/src/app/messages/messages.service.ts
@@ -1,14 +1,19 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessagesService {
+  messages$ = new BehaviorSubject<string[]>([]);
 
-  private messages = signal<string[]>([]);
-  allMessages = this.messages.asReadonly();
+  private messages: string[] = [];
+  get allMessages() {
+    return [...this.messages ];
+  }
 
   addMessage(message: string) {
-    this.messages.update((prevMessages) => [...prevMessages, message]);
+    this.messages = [ ... this.messages, message];
+    this.messages$.next([...this.allMessages]);
   }
 }
