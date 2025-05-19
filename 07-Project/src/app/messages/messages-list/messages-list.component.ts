828537c707ddc4665d4486bd0d2ad148bbbab894
Mitesh <miteshprajapati6439.com>
2025-05-19 16:19:34 +0530

    signals with zoneless detection

--- 07-Project/src/app/messages/messages-list/messages-list.component.ts
+++ 07-Project/src/app/messages/messages-list/messages-list.component.ts
@@ -5,14 +5,13 @@ import { AsyncPipe } from '@angular/common';
 @Component({
   selector: 'app-messages-list',
   standalone: true,
-  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  messages$ = this.messagesService.messages$;
+  messages = this.messagesService.allMessages;
 
 
   // private cdRef = inject(ChangeDetectorRef);
