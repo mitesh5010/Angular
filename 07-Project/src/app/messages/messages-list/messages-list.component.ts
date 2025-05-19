1a894c587dd4c264394bfb71f1b50c8f17c8ee6c
Mitesh <miteshprajapati6439.com>
2025-05-19 16:04:33 +0530

    use Async pipe

--- 07-Project/src/app/messages/messages-list/messages-list.component.ts
+++ 07-Project/src/app/messages/messages-list/messages-list.component.ts
@@ -1,29 +1,34 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component,  inject } from '@angular/core';
 import { MessagesService } from '../messages.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-messages-list',
   standalone: true,
+  imports: [AsyncPipe],
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MessagesListComponent implements OnInit {
+export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef);
-  messages: string[] = [];
+  messages$ = this.messagesService.messages$;
 
-  ngOnInit() {
-    const subscription = this.messagesService.messages$.subscribe((messages)=>{
-      this.messages = messages;
-      this.cdRef.markForCheck();// Mark the component for check
-    });
-    // Cleanup subscription on destroy
-    this.destroyRef.onDestroy(()=>{
-      subscription.unsubscribe();
-    })
-  }
+
+  // private cdRef = inject(ChangeDetectorRef);
+  // private destroyRef = inject(DestroyRef);
+  // messages: string[] = [];
+
+  // ngOnInit() {
+  //   const subscription = this.messagesService.messages$.subscribe((messages)=>{
+  //     this.messages = messages;
+  //     this.cdRef.markForCheck();// Mark the component for check
+  //   });
+  //   // Cleanup subscription on destroy
+  //   this.destroyRef.onDestroy(()=>{
+  //     subscription.unsubscribe();
+  //   })
+  // }
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
