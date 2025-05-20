d20e1e60029c4643b904cd82cfe0f2a7ce26cc90
Mitesh <miteshprajapati6439.com>
2025-05-20 17:21:51 +0530

    Showing Loading Fallback and Handling Errors

--- 09-Project/src/app/places/available-places/available-places.component.ts
+++ 09-Project/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,36 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('')
+  private httpclient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    this.isFetching.set(true);
+    const subscription = this.httpclient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((resData)=> resData.places),
+        catchError((error) => {
+          console.log(error);
+          return throwError(()=> new Error('Somthing went wrong! Please try again later.'))
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          console.log(error);
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        }
+      });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
