3b4d6b505215fdfd9381f198686e08afe9047e37
Mitesh <miteshprajapati6439.com>
2025-05-20 13:08:55 +0530

    Learn RxJS-Observable With Project-8

---
+++ 08-Project/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { interval, map } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html'
+})
+export class AppComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscribetion = interval(1000).pipe(
+      map((val)=> val*2)
+    ).subscribe({
+      next: (val) => console.log(val)
+    });
+
+    this.destroyRef.onDestroy(()=>{
+      subscribetion.unsubscribe();
+    })
+  }
+}
