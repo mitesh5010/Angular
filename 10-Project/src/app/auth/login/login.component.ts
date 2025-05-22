3310196477edcd926ab6a13a85bee8475b23d388
Mitesh <miteshprajapati6439.com>
2025-05-22 17:02:37 +0530

    login - interacting with form with reactive approch

--- 10-Project/src/app/auth/login/login.component.ts
+++ 10-Project/src/app/auth/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { debounceTime, of } from 'rxjs';
 
-function mustContainMark(control:AbstractControl) {
+
+function mustContainMark(control: AbstractControl) {
   if (control.value.includes('?')) {
     return null;
   }
 
-  return { doesNotContainMark: true }
+  return { doesNotContainMark: true };
+}
+
+function uniqeEmail(control: AbstractControl) {
+  if (control.value !== 'test@example.com') {
+    return of(null);
+  }
+  return of({ notUniqueEmail: true });
 }
 
 @Component({
@@ -16,26 +31,62 @@ function mustContainMark(control:AbstractControl) {
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
   form = new FormGroup({
     email: new FormControl('', {
-      validators: [Validators.required, Validators.email]
+      validators: [Validators.required, Validators.email],
+      asyncValidators: [uniqeEmail],
     }),
     password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6), mustContainMark]
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        mustContainMark,
+      ],
     }),
-  })
+  });
 
   get emailIsValid() {
-    return this.form.controls.email.touched &&
-           this.form.controls.email.invalid &&
-           this.form.controls.email.dirty
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.invalid &&
+      this.form.controls.email.dirty
+    );
   }
 
   get passwordIsValid() {
-    return this.form.controls.password.touched &&
-           this.form.controls.password.invalid &&
-           this.form.controls.password.dirty
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.invalid &&
+      this.form.controls.password.dirty
+    );
+  }
+
+  ngOnInit(): void {
+
+    const savedForm = window.localStorage.getItem('saved-login-form');
+    if (savedForm) {
+      const loadedForm = JSON.parse(savedForm);
+      this.form.patchValue({
+        email: loadedForm.email,
+      })
+    }
+
+    const subscribtion = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => {
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          );
+        },
+      });
+    this.destroyRef.onDestroy(() => {
+      subscribtion.unsubscribe();
+    });
   }
 
   onSubmit() {
@@ -44,7 +95,5 @@ export class LoginComponent {
     const enteredPassword = this.form.value.password;
 
     console.log(enteredEmail, enteredPassword);
-    
-    
   }
 }
