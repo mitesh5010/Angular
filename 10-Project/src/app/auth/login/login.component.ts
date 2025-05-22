44f7a5d385481d95d1ef2ad06ca5f1d5a1c59e98
Mitesh <miteshprajapati6439.com>
2025-05-22 15:57:33 +0530

    build custom validator

--- 10-Project/src/app/auth/login/login.component.ts
+++ 10-Project/src/app/auth/login/login.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+function mustContainMark(control:AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+
+  return { doesNotContainMark: true }
+}
 
 @Component({
   selector: 'app-login',
@@ -14,7 +22,7 @@ export class LoginComponent {
       validators: [Validators.required, Validators.email]
     }),
     password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6)]
+      validators: [Validators.required, Validators.minLength(6), mustContainMark]
     }),
   })
 
