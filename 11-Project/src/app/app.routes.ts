7f723950c2cfdc8d6bdad71e2675a2ad80f37d7e
Mitesh <miteshprajapati6439@gmail.com>
2025-05-26 14:44:39 +0530

    Create Dummy Route Guard

--- 11-Project/src/app/app.routes.ts
+++ 11-Project/src/app/app.routes.ts
@@ -1,9 +1,20 @@
-import {  Routes } from "@angular/router";
+import {  CanMatchFn, RedirectCommand, Router, Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { resolveTitle, resolveUserName, UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { inject } from "@angular/core";
+
+const dummyCanMatch: CanMatchFn = (route, segments) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess > 0.5) {
+    return true;
+  }
+
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
+}
 
 export const routes : Routes = [
   {
@@ -32,7 +43,8 @@ export const routes : Routes = [
     resolve: {
       userName : resolveUserName, // Resolve the user name before activating the route
     },
-    title: resolveTitle // Resolve the title before activating the route
+    title: resolveTitle, // Resolve the title before activating the route
+    canMatch: [dummyCanMatch],
 
   },
   {
