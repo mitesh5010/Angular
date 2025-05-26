2e443682a398b1e056127a477831d11fb37a8373
Mitesh <miteshprajapati6439@gmail.com>
2025-05-26 12:33:18 +0530

    Route the invalid input url

--- 11-Project/src/app/app.routes.ts
+++ 11-Project/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TasksComponent } from "./tasks/tasks.component";
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 export const routes : Routes = [
   {
@@ -14,6 +15,11 @@ export const routes : Routes = [
     component: UserTasksComponent,
     children: [
       {
+        path: '', // <domain>/users/<uID> (default child route)
+        redirectTo: 'tasks', // Redirect to the tasks route
+        pathMatch: 'full', // Match the full path
+      },
+      {
         path: 'tasks', // <domain>/users/<uID>/tasks
         component: TasksComponent,
       },
@@ -22,5 +28,9 @@ export const routes : Routes = [
         component: NewTaskComponent,
       }
     ],
+  },
+  {
+    path: '**', // Wildcard route for a 404 page
+    component: NotFoundComponent,
   }
 ]
